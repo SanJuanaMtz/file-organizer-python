cf356398dfdcfe4ff0684d8baee7db332d95b534
SanJuanaMtz <55932354+SanJuanaMtz@users.noreply.github.com>
2026-05-16 15:08:03 +0000

    Refactored file organized using dictionaries and improved file handing

--- v2/organizador_v2.py
+++ v2/organizador_v2.py
@@ -1,1 +1,33 @@
+import os
+import shutil
 
+carpeta = "Downloads"
+
+CARPETAS = {
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Pictures": [".jpg", ".jpeg", ".png"],
+    "Compressed": [".zip", ".rar"]
+}
+
+for archivo in os.listdir(carpeta):
+
+    ruta_archivo = os.path.join(carpeta, archivo)
+
+    if os.path.isfile(ruta_archivo):
+
+        extension = os.path.splitext(archivo)[1].lower()
+
+        for carpeta_destino, extensiones in CARPETAS.items():
+
+            if extension in extensiones:
+
+                os.makedirs(carpeta_destino, exist_ok=True)
+
+                shutil.move(
+                    ruta_archivo,
+                    os.path.join(carpeta_destino, archivo)
+                )
+
+                print(f"Moved: {archivo} → {carpeta_destino}")
+
+                break
