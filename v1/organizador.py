e07acb88eb90c667feeda70295fe9a5391312424
San Martinez <55932354+SanJuanaMtz@users.noreply.github.com>
2026-05-28 10:19:46 -0500

    Update organizador.py

--- v1/organizador.py
+++ v1/organizador.py
@@ -1,1 +1,17 @@
+import os #Read files
+import shutil  #Move those files
 
+carpeta = "Downloads"
+
+for archivo in os.listdir(carpeta):
+    if archivo.endswith(".pdf"):
+        os.makedirs("Documents", exist_ok=True)
+        shutil.move(f"{carpeta}/{archivo}", "Documents")
+
+    elif archivo.endswith(".jpg"):
+        os.makedirs("Pictures", exist_ok=True)
+        shutil.move(f"{carpeta}/{archivo}", "Pictures")
+
+    elif archivo.endswith(".docx"):
+        os.makedirs("Documents", exist_ok=True)
+        shutil.move(f"{carpeta}/{archivo}", "Documents")
